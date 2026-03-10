58fb1b4fc81cbf91c4322c96ea1c9013ebe1aa5f
eryk <e.szczepaniec@lightpixel.io>
2026-03-10 17:41:27 +0100

    feat: display dynamically form steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from './layouts/Form';
+import FormNavigation from './layouts/FormNavigation';
 const App: React.FC = () => {
+  const [step, setStep] = useState<number>(1);
+
   return (
-    <div className="mx-auto max-w-[800px]">
-      <Form />
+    <div className="w-[90%] mx-auto max-w-[800px]">
+      <div className="mx-auto max-w-[600px] ">
+        <Form step={step} />
+        <FormNavigation step={step} />
+      </div>
     </div>
   );
 };
--- src/layouts/Form.tsx
+++ src/layouts/Form.tsx
@@ -1,49 +1,11 @@
-import React, { useState } from 'react';
-const Form: React.FC = () => {
-  const [email, setEmail] = useState<string>('');
-  const [name, setName] = useState<string>('');
-  const [number, setNumber] = useState<string>('');
+import React from 'react';
+import FirstStep from './FirstStep';
+interface FormNavigationProps {
+  step: number;
+}
 
-  return (
-    <form className="p-6 mx-auto w-[90%] max-w-[600px] rounded-lg bg-white">
-      <h1 className="text-2xl font-semibold">Personal info</h1>
-      <p className="my-2 text-gray-400">
-        Please provide your name, email address, and phone number.
-      </p>
-      <label className="block mb-4" htmlFor="">
-        Name
-        <input
-          type="text"
-          placeholder="e.g. Stephen King"
-          className="p-2 w-full border-2 border-gray-200 rounded-md"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />{' '}
-      </label>
-
-      <label className="block mb-4" htmlFor="">
-        Email Address
-        <input
-          type="email"
-          placeholder="e.g. stephenking@lorem.com"
-          className="p-2 w-full border-2 border-gray-200 rounded-md"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />{' '}
-      </label>
-
-      <label className="block mb-4" htmlFor="">
-        Phone Number
-        <input
-          type="text"
-          placeholder="e.g. +1 234 567 890"
-          className="p-2 w-full border-2 border-gray-200 rounded-md"
-          onChange={(e) => setNumber(e.target.value)}
-          value={number}
-        />{' '}
-      </label>
-    </form>
-  );
+const Form: React.FC<FormNavigationProps> = ({ step }) => {
+  return <form className="p-6 rounded-lg bg-white">{step === 1 && <FirstStep />}</form>;
 };
 
 export default Form;
