1fe6070a75298c605936cba7a7ad39c3c6500dfc
eryk <e.szczepaniec@lightpixel.io>
2026-03-11 08:58:41 +0100

    feat: add skeleton for third step

---
+++ src/layouts/ThirdStep.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+const ThirdStep: React.FC = () => {
+  return (
+    <>
+      <h1 className="text-2xl md:text-3xl font-semibold">Pick add-ons</h1>
+      <p className="mt-2 mb-4 md:mb-[2rem] text-gray-400">
+        Add-ons help enhance your gaming experience.
+      </p>
+      <div></div>
+    </>
+  );
+};
+
+export default ThirdStep;
