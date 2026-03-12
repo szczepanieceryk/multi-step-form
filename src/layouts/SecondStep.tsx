ecd6d6a4830d6abd53e06d316242d5a4f3bf45d1
eryk <e.szczepaniec@lightpixel.io>
2026-03-12 16:17:19 +0100

    feat(ui): style active class on buttons

--- src/layouts/SecondStep.tsx
+++ src/layouts/SecondStep.tsx
@@ -4,6 +4,7 @@ import advancedIcon from '../assets/images/icon-advanced.svg';
 import proIcon from '../assets/images/icon-pro.svg';
 
 const SecondStep: React.FC = () => {
+  const activeClass = 'border-[#473dffff] bg-[#473dffff]/10';
   return (
     <>
       <h1 className="text-2xl md:text-3xl font-semibold">Select your plan</h1>
@@ -11,7 +12,12 @@ const SecondStep: React.FC = () => {
         You have the option of monthly or yearly billing.
       </p>
       <div className="md:flex justify-between gap-4">
-        <button className="mb-4 p-4 md:p-2 flex flex-wrap w-full md:max-w-[150px] border-2 border-gray-300 rounded-md">
+        {/** Button */}
+
+        <button
+          className={`mb-4 p-4 md:p-2 flex flex-wrap w-full md:max-w-[150px] border-2 border-gray-300 hover:border-[#473dffff] hover:bg-[#473dffff]/10 ${activeClass} rounded-md`}
+          type="button"
+        >
           <img src={arcadeIcon} alt="" className="w-10 h-10 md:basis-full md:mb-[3rem]" />
           <div className="ml-2 flex flex-wrap text-left">
             <span className="block basis-full">Arcade</span>
@@ -19,7 +25,13 @@ const SecondStep: React.FC = () => {
             <span className="hidden md:block basis-full text-blue-700">2 months free</span>
           </div>
         </button>
-        <button className="mb-4 p-4 md:p-2 flex flex-wrap w-full md:max-w-[150px] border-2 border-gray-300 rounded-md">
+        {/** Button */}
+
+        {/** Button */}
+        <button
+          className="mb-4 p-4 md:p-2 flex flex-wrap w-full md:max-w-[150px] border-2 border-gray-300 hover:border-[#473dffff] rounded-md"
+          type="button"
+        >
           <img src={advancedIcon} alt="" className="w-10 h-10 md:basis-full md:mb-[3rem]" />
           <div className="ml-2 flex flex-wrap text-left">
             <span className="block basis-full">Advanced</span>
@@ -27,7 +39,13 @@ const SecondStep: React.FC = () => {
             <span className="hidden md:block basis-full text-blue-700">2 months free</span>
           </div>
         </button>
-        <button className="mb-4 p-4 md:p-2 flex flex-wrap  w-full md:max-w-[150px] border-2 border-gray-300 rounded-md">
+        {/** Button */}
+
+        {/** Button */}
+        <button
+          className="mb-4 p-4 md:p-2 flex flex-wrap  w-full md:max-w-[150px] border-2 border-gray-300 hover:border-[#473dffff] rounded-md"
+          type="button"
+        >
           <img src={proIcon} alt="" className="w-10 h-10 md:basis-full md:mb-[3rem]" />
           <div className="ml-2 flex flex-wrap text-left">
             <span className="block basis-full">Pro</span>
@@ -35,6 +53,7 @@ const SecondStep: React.FC = () => {
             <span className="hidden md:block basis-full text-blue-700">2 months free</span>
           </div>
         </button>
+        {/** Button */}
       </div>{' '}
       <div className="p-4 flex flex-wrap justify-center bg-blue-100/50 rounded-md">
         <span>
